86220af7b999d439040dc778ef0d22333be2c2f3
9node <rayyan@theninthnode.com>
2025-01-19 17:32:22 +0530

    reverted to template changes for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,9 @@ import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  cleanDistDir: true,
-  trailingSlash: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true  // Important for static export
-  }
 }
 
 const withMDX = nextMDX({
