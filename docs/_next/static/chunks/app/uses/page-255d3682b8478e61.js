c35b36332bc4f9bb2e7c054532ef92950ef69cac
Rayyan Nafees <59685062+RayyanNafees@users.noreply.github.com>
2025-03-12 00:03:33 +0000

    just redirects to main site now

--- docs/_next/static/chunks/app/uses/page-255d3682b8478e61.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[453,160,701],{7e3:function(e,n,u){Promise.resolve().then(u.t.bind(u,6685,23))}},function(e){e.O(0,[685,971,596,744],function(){return e(e.s=7e3)}),_N_E=e.O()}]);
